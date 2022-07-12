a8342ba3b67fb7553ac0e827cedec0f3f39ba379
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-07-12 16:54:04 +0530

    Fix:useing map function creating new modify arry

---
+++ filter.js
@@ -0,0 +1,9 @@
+ arr=[{id:"1",name:"swapnil",age:25,address:"pune"},
+{id:"2",name:"anu",age:25,address:"barshi"},
+{id:"3",name:"madhu",age:25,address:"nanded"},
+{id:"14",name:"kunal",age:23,address:"pune"},
+{id:"15",name:"ganu",age:23,address:"solapur"},
+{id:"16",name:"sham",age:22,address:"pune"}];
+const swa=()=>{
+    console.log("hi");
+}
