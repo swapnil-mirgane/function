bc3d0828e49dba006a169897aa1ae99af7662e97
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-07-13 16:16:38 +0530

    created promoise with example

--- promise.js
+++ promise.js
@@ -1,13 +1,45 @@
-let str=["anuj","pune","barshi"];
-function sw(){
-    str.map((ele)=>{console.log(ele);})
+// let str=["anuj","pune","barshi"];
+// function sw(){
+//     str.map((ele)=>{console.log(ele);})
+// }
+// setTimeout(sw(), 1000);
+
+// setTimeout(() => {
+//     str.push("delli");
+//     sw();   
+// }, 2000);
+
+// var a=str.shift();
+// console.log(a);
+
+// let b=new Promise((resolve,reject)=>{
+//     let a=false;
+//     if(true){
+//         resolve("it is reslove");
+//     }else
+//     {
+//         reject("promise is rejected");
+//     }
+
+// });
+// b.then((res)=>{
+//     console.log("promise status is:"+res);
+// }).catch((err)=>{
+//     console.log("erro"+err);
+// })
+
+
+
+var a=new Promise((resolve,reject)=>{
+var d=false;
+if(d){
+    resolve("madhuri")
+}else{
+    reject("vaibhav")
 }
-setTimeout(sw(), 1000);
-
-setTimeout(() => {
-    str.push("delli");
-    sw();   
-}, 2000);
-
-var a=str.shift();
-console.log(a);
+});
+a.then((m)=>{
+    console.log("good girl"+m);
+}).catch((err)=>{
+    console.log("good  boy" +" "+err);
+})
